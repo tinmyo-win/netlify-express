6da6aceb2454af07f7b28b5a5667df8b60243044
TinMyo Win <tinmyowin@utycc.edu.mm>
2022-09-05 10:10:48 +0630

    Deploy express on netlify

---
+++ src/api.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const serverless = require('serverless-http')
+
+const app = express()
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.json({
+        'hello': 'Hi'
+    })
+});
+
+router.get('/test', (req, res) => {
+    res.json({
+        'test': "Sending testing"
+    })
+})
+
+app.use('/api', router)
+
+module.exports.handler = serverless(app)
